f81dadd2770addc8ff72a8cb8d80c30d71ae5629
Mayank-Ydv <ymayank832@gmail.com>
2024-04-27 20:45:18 +0530

    On branch main

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin:"https://e-learning-frontend-et24wy978-mayank-yadavs-projects-edded3a9.vercel.app",
+        origin:"*",
         credentials:true
     })
 )
