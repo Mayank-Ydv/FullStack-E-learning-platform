a8ed35338e8f65a4b0429c9bf18f3b172512a7c6
Mayank-Ydv <ymayank832@gmail.com>
2024-08-08 01:03:51 +0530

    On branch main

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -26,7 +26,8 @@ exports.resetPasswordToken = async (req, res) => {
       { new: true }
     );
     //create url
-    const url = `http://localhost:3000/update-password/${token}`;
+    // const url = `http://localhost:3000/update-password/${token}`;
+    const url = `https://e-learning-frontend-sigma.vercel.app/update-password/${token}`;
     //send mail containing the url
     await mailSender(
       email,
